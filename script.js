5b5ccc53c269e6ae7223141edf9742296e6ce294
Edwin Chen <edwinchen85@gmail.com>
2017-05-29 10:17:26 +0800

    Define and initialize variables

--- script.js
+++ script.js
@@ -18,3 +18,7 @@ function inIframe () {
   try { return window.self !== window.top }
   catch (e) { return true; }
 }
+
+  var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', '#472E32', '#BDBB99', '#77B1A9', '#73A857'];
+  var currentQuote = '';
+  var currentAuthor = '';
