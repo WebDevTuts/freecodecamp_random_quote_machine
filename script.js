8e5d5243aa7570f566d785c7eec2f2b354474c5d
Edwin Chen <edwinchen85@gmail.com>
2017-05-29 10:05:43 +0800

    Share quote when tweeter button is clicked

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
   getQuote();
   $('new-quote').on('click', getQuote);
+  $('tweet-quote').on('click', function() {
+    if(!inIframe()) {
+      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+    }
+  });
 });
